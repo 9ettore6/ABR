ad6a6f59fefbc8ab38d523aac5d96ebe567d9b9b
Ettore <ettorecelozzi@gmail.com>
2017-05-16 09:33:13 +0200

    aggiunta ereditarietà e vari metodi

--- ABR.py
+++ ABR.py
@@ -1,41 +1,36 @@
-import math
-import random
-
-
 class Node:
-    def __init__(self, key):
-        self.key = key
-        self.left = None
-        self.right = None
-        self.padre = None
+    def __init__(self, key, nil):
+        if not key:
+            self.key = "nil"
+            self.left = self
+            self.right = self
+            self.p = self
+        else:
+            self.key = key
+            self.left = nil
+            self.right = nil
+            self.p = nil
 
-    def get(self):
-        return self.key
+    def setP(self, p):
+        self.p = p
 
-    def set(self, key):
-        self.key = key
+    def getP(self):
+        return self.p
 
-    def setp(self, p):
-        self.padre = p
-
-    def getp(self):
-        return self.padre
-
-    def getChildren(self):
-        children = []
-        if self.left != None:
-            children.append(self.left)
-        if self.right != None:
-            children.append(self.right)
-        return children
+    def height(self, node):
+        if node.key == "nil":
+            return 0
+        else:
+            return max(self.height(node.left), self.height(node.right)) + 1
 
 
 class ABR:
     def __init__(self):
-        self.root = None
+        self.nil = Node(None, None)
+        self.root = self.nil
 
     def setRoot(self, key):
-        self.root = Node(key)
+        self.root = Node(key, self.nil)
 
     def changeRoot(self, node):
         self.root = node
@@ -43,56 +38,53 @@ class ABR:
     def getRoot(self):
         return self.root
 
-    def insert(self, key):
-        if self.root is None:
-            self.setRoot(key)
-            a = self.treesearch(key, self.root)
-            a.padre = None  # self.previousnode(key, self.root)
-        else:
-            self.insertNode(self.root, key)
+    def treeminimum(self, x):
+        while x.left.key is not "nil":
+            x = x.left
+        return x
 
-    def insertNode(self, currentNode, key):
+    def treemaximum(self, x):
+        while x.right.key is not "nil":
+            x = x.right
+        return x
+
+    def insert(self, key):
+        if self.root is self.nil:
+            self.setRoot(key)
+            self.root.setP(self.nil)
+            return self.root
+        else:
+            return self.insertNode(key, self.root)
+
+    def insertNode(self, key, currentNode):
         if key <= currentNode.key:
-            if currentNode.left:
-                self.insertNode(currentNode.left, key)
+            if currentNode.left.key != "nil":
+                self.insertNode(key, currentNode.left)
             else:
-                currentNode.left = Node(key)
-                currentNode.left.setp(currentNode)
+                currentNode.left = Node(key, self.nil)
+                currentNode.left.setP(currentNode)
                 return currentNode.left
         elif key > currentNode.key:
-            if currentNode.right:
-                self.insertNode(currentNode.right, key)
+            if currentNode.right.key != "nil":
+                self.insertNode(key, currentNode.right)
             else:
-                currentNode.right = Node(key)
-                currentNode.right.setp(currentNode)
+                currentNode.right = Node(key, self.nil)
+                currentNode.right.setP(currentNode)
                 return currentNode.right
-
-    def find(self, key):
-        return self.findNode(self.root, key)
-
-    def findNode(self, currentNode, key):
-        if currentNode is None:
-            return False
-        elif key == currentNode.key:
-            return True
-        elif key < currentNode.key:
-            return self.findNode(currentNode.left, key)
-        else:
-            return self.findNode(currentNode.right, key)
 
     def inorder(self):
         def _inorder(v):
-            if v is None:
+            if v.key == "nil":
                 return
-            if v.left is not None:
+            if v.left.key != "nil":
                 _inorder(v.left)
             print v.key
-            if v.right is not None:
+            if v.right.key != "nil":
                 _inorder(v.right)
 
-        _inorder(self.root)
+            _inorder(self.root)
 
-    def treesearch(self, k, currentNode):  # restituisce un ptr a un nodo con chiave k se esiste
+    def treeSearch(self, k, currentNode):
         while currentNode and k != currentNode.key:
             if k < currentNode.key:
                 currentNode = currentNode.left
@@ -102,33 +94,24 @@ class ABR:
             return currentNode
         print "Elemento non presente"
 
-    '''def previousnode(self, k, currentNode):  # restituisce il ptr al nodo precendente
-        while currentNode and k != currentNode.key:
-            if k < currentNode.key:
-                currentptr = currentNode
-                currentNode = currentNode.left
-            else:
-                currentptr = currentNode
-                currentNode = currentNode.right
-        if currentNode:
-            return currentptr
-        print "Precedente non presente"
-'''
-
-def main():
-    tree = ABR()
-    tree.insert(4)
-    tree.insert(8)
-    tree.insert(15)
-    tree.insert(7)
-    tree.insert(5)
-    print tree.treesearch(5, tree.root).padre.key
-    print ""
-    tree.inorder()
-    a = tree.treesearch(7, tree.root)
-    b = tree.previousnode(8, tree.root)
-    print b.key
+    def serialize(self, node):
+        print "", node.key, "(",
+        if node.left.key is not "nil":
+            self.serialize(node.left)
+        else:
+            print "_",
+        if node.right.key is not "nil":
+            self.serialize(node.right)
+        else:
+            print" _ ) ,",
 
 
-if __name__ == "__main__":
-    main()
+tre = ABR()
+tre.insert(8)
+tre.insert(7)
+tre.insert(4)
+tre.insert(3)
+tre.insert(9)
+a = tre.treemaximum(tre.getRoot())
+print a.key
+tre.serialize(tre.getRoot())
--- RB.py
+++ RB.py
@@ -1,40 +1,143 @@
-from ABR import ABR
+import ABR
 
 
-class RB(ABR):
-    def __init__(self):
-        ABR.__init__(self)
-        self.color = "Black"
+class RBNode(ABR.Node):
+    def __init__(self, key, nil):
+        ABR.Node.__init__(self, key, nil)
+        self.color = "black"
 
-    def getcolor(self):
+    def getColor(self):
         return self.color
 
-    def left_routate(self, tree, x):  # si suppone che x.right != T.nil
+    def setColor(self, color):
+        self.color = color
+
+
+class RBTree(ABR.ABR):
+    def __init__(self):
+        ABR.ABR.__init__(self)
+        self.nil = RBNode(None, None)
+        self.nil.setColor("black")
+        self.root = self.nil
+
+    def setRoot(self, key):
+        self.root = RBNode(key, self.nil)
+
+    def left_rotate(self, x):
         y = x.right
         x.right = y.left
-        if y.left != None:
-            y.left.padre = x
-        y.padre = x.padre
-        if x.padre == None:
-            tree.changeRoot(y)
-        elif x == x.padre.left:
-            x.padre.left = y
+        if y.left.key != "nil":
+            y.left.setP(x)
+        y.setP(x.getP())
+        if x.getP().key == "nil":
+            self.changeRoot(y)
+        elif x == x.getP().left:
+            x.getP().left = y
         else:
-            x.padre.right = y
+            x.getP().right = y
         y.left = x
-        x.padre = y
+        x.setP(y)
+
+    def right_rotate(self, x):
+        y = x.left
+        x.left = y.right
+        if y.right.key != "nil":
+            y.right.setP(x)
+        y.setP(x.getP())
+        if x.getP().key == "nil":
+            self.changeRoot(y)
+        elif x == x.getP().left:
+            x.getP().left = y
+        else:
+            x.getP().right = y
+        y.right = x
+        x.setP(y)
+
+    def insert(self, key):
+        if self.root is self.nil:
+            self.setRoot(key)
+            self.root.setColor("red")
+            self.fixup(self.root)
+        else:
+            self.insertNode(key, self.root)
+
+    def insertNode(self, key, currentNode):
+        if key <= currentNode.key:
+            if currentNode.left.key != "nil":
+                self.insertNode(key, currentNode.left)
+            else:
+                currentNode.left = RBNode(key, self.nil)
+                currentNode.left.setP(currentNode)
+                currentNode.left.setColor("red")
+                self.fixup(currentNode.left)
+                return currentNode.left
+        elif key > currentNode.key:
+            if currentNode.right.key != "nil":
+                self.insertNode(key, currentNode.right)
+            else:
+                currentNode.right = RBNode(key, self.nil)
+                currentNode.right.setP(currentNode)
+                currentNode.right.setColor("red")
+                self.fixup(currentNode.right)
+                return currentNode.right
+
+    def inorder(self):
+        def _inorder(v):
+            if v.key == "nil":
+                return
+            if v.left.key != "nil":
+                _inorder(v.left)
+            print v.key, " ", v.getColor()
+            if v.right.key != "nil":
+                _inorder(v.right)
+
+        _inorder(self.root)
+
+    def fixup(self, z):
+        while z.getP().getColor() == "red":
+            if z.getP() == z.getP().getP().left:
+                y = z.getP().getP().right
+                if y.getColor() == "red":
+                    z.getP().setColor("black")
+                    y.setColor("black")
+                    z.getP().getP().setColor("red")
+                    z = z.getP().getP()
+                else:
+                    if z == z.getP().right:
+                        z = z.getP()
+                        self.left_rotate(z)
+                    z.getP().setColor("black")
+                    z.getP().getP().setColor("red")
+                    self.right_rotate(z.getP().getP())
+            else:
+                y = z.getP().getP().left
+                if y.color == "red":
+                    z.getP().setColor("black")
+                    y.setColor("black")
+                    z.getP().getP().setColor("red")
+                    z = z.getP().getP()
+                else:
+                    if z == z.getP().left:
+                        z = z.getP()
+                        self.right_rotate(z)
+                    z.getP().setColor("black")
+                    z.getP().getP().setColor("red")
+                    self.left_rotate(z.getP().getP())
+        self.root.setColor("black")
 
 
-MyRBTree = RB()
-MyRBTree.insert(3)
-MyRBTree.insert(1)
-MyRBTree.insert(2)
-MyRBTree.insert(7)
-MyRBTree.insert(5)
-MyRBTree.insert(9)
-print MyRBTree.getRoot().key
-print ""
-test = MyRBTree.treesearch(3, MyRBTree.getRoot())
-MyRBTree.left_routate(MyRBTree, test)
-print "Radice: ", MyRBTree.getRoot().key
-print MyRBTree.getRoot().right.key
+rbtre = RBTree()
+rbtre.insert(4)
+rbtre.insert(5)
+rbtre.insert(2)
+rbtre.insert(1)
+rbtre.insert(3)
+rbtre.insert(8)
+rbtre.insert(6)
+rbtre.inorder()
+a = rbtre.treeSearch(4,rbtre.getRoot())
+print "padre:", a.getP().key
+print "figlio:", a.left.key
+print rbtre.getRoot().key
+rbtre.left_rotate(a)
+print rbtre.getRoot().key
